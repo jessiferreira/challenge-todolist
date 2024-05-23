fbb15a70f26caa84842cd9839076dbbf977fb3d3
Jéssica <ferrsjessy@gmail.com>
2024-05-23 14:49:49 -0300

    feat: Create Todo controller.

---
+++ challenge-todolist/src/main/java/br/com/jessicaferreira/challenge_todolist/controller/TodoController.java
@@ -0,0 +1,38 @@
+package br.com.jessicaferreira.challenge_todolist.controller;
+
+import br.com.jessicaferreira.challenge_todolist.entity.Todo;
+import br.com.jessicaferreira.challenge_todolist.service.TodoService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+    private TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @PostMapping
+    List<Todo> create(@RequestBody @Valid Todo todo) {
+        return todoService.create(todo);
+    }
+
+    @GetMapping
+    List<Todo> list() {
+        return todoService.list();
+    }
+
+    @PutMapping
+    List<Todo> update(@RequestBody Todo todo) {
+        return todoService.update(todo);
+    }
+
+    @DeleteMapping("{id}")
+    List<Todo> delete(@PathVariable("id") Long id) {
+        return todoService.delete(id);
+    }
+}
